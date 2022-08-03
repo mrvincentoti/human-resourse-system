772373f6db151980870711a4fb866b05b6d3e766
vincentoti <vincent.oti@layer3.com.ng>
2022-08-03 14:13:01 +0100

    Added job to create s3 bucket with terraform

--- .circleci/files/terraform/frontend.tf
+++ .circleci/files/terraform/frontend.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+        }
+    }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
 resource "aws_s3_bucket" "front-end" {
   bucket = "udapeople-${var.bucket_name}"
   tags = {
