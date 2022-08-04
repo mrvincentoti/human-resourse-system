1f42ed6662a24fc7b325446aa6ba15bdc9d42787
vincentoti <vincent.oti@layer3.com.ng>
2022-08-04 15:30:47 +0100

    configured s3 for website hosting

--- .circleci/files/terraform/frontend.tf
+++ .circleci/files/terraform/frontend.tf
@@ -3,8 +3,8 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 3.0"
-        }
     }
+  }
 }
 
 provider "aws" {
@@ -13,7 +13,34 @@ provider "aws" {
 
 resource "aws_s3_bucket" "front-end" {
   bucket = "udapeople-${var.bucket_name}"
+  acl    = "public-read" 
+  website {    
+    index_document = "index.html"    
+    error_document = "404.html"
+  }
   tags = {
     Environment = var.environment_name
-    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "front-end" {  
+  bucket = aws_s3_bucket.front-end.id   
+  policy = <<POLICY
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Sid": "PublicReadForGetBucketObjects",
+        "Effect": "Allow",
+        "Principal": "*",
+        "Action": [
+          "s3:GetObject"
+        ],
+        "Resource": [
+          "arn:aws:s3:::${aws_s3_bucket.front-end.id}/*"
+        ]
+      }
+    ]
+  }
+  POLICY
 }
